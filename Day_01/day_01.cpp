79ab59eae6595533f66f59b3de36f487f0761fd9
Kemoneart Ou <kemoneart@gmail.com>
2025-12-03 14:44:59 +1300

    Day 1 (Part 1)

--- Day_01/day_01.cpp
+++ Day_01/day_01.cpp
@@ -6,9 +6,10 @@ fstream inputFile;
 string input, fileName;
 int temp, num, zeros, len;
 
+/* Part 1 */
+
 int main()
 {
-
     fileName = "elf.txt";
     num = 50;
     zeros = 0;
@@ -21,20 +22,21 @@ int main()
     }
 
     while(inputFile >> input)
-    {
-    
+    {    
         temp = 0;
         len = input.length();
 
-        for(int i = 1; i < len; i++){
+        for(int i = 1; i < len; i++)
+        {
             temp = (temp * 10) + (input[i] - 48);
         }
-        
         if(input[0] == 'L')
             num = num - temp;
         else if (input[0] == 'R')
             num = num + temp;
+            
         num = num % 100;
+
         if(num < 0)
             num = 100 - abs(num);
             
@@ -42,9 +44,7 @@ int main()
         
         if(num == 0)
             zeros++;
-
     }
-
     cout << "zeros = " << zeros << endl; 
 }
 
